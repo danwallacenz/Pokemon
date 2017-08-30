8943cedd1eb62f5dc0f453b6cf4748c72baa586f
Dan Wallace <daniel.wallace.nz@gmail.com>
2017-08-30 22:38:52 +1200

    Load all Pokemon names

---
+++ Pokemon/NetworkLoader.swift
@@ -0,0 +1,34 @@
+//
+//  NetworkLoader.swift
+//  Pokemon
+//
+//  Created by Daniel Wallace on 30/08/17.
+//  Copyright © 2017 danwallacenz. All rights reserved.
+//
+
+import Foundation
+
+class NetworkLoader {
+    
+    static func loadAllPokemon(completion: @escaping (String) -> ()) {
+        if let url = URL(string: "https://pokeapi.co/api/v2/pokemon") {
+            (URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if let error = error {
+                    print("Error: \(error)")
+                    
+                } else if let response = response,
+                    let data = data,
+                    let results = String(data: data, encoding: .utf8) {
+                        print("Response: \(response)")
+                        print("DATA:\n\(results)\nEND DATA\n")
+                        completion(results)
+                }
+            }).resume()
+        }
+    }
+    
+    static func loadPokemon(withId id: String) {
+        
+    }
+    
+}
--- Pokemon/ViewController.swift
+++ Pokemon/ViewController.swift
@@ -13,13 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        NetworkLoader.loadAllPokemon { [weak self] results in
+            print(results)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
