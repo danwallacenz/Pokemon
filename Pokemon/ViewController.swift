14790fec9e6c34cc33fad63c6b37cfe8150f5bb9
Dan Wallace <daniel.wallace.nz@gmail.com>
2017-09-01 20:14:20 +1200

    populate table

--- Pokemon/ViewController.swift
+++ Pokemon/ViewController.swift
@@ -8,14 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        NetworkLoader.loadAllPokemon { results in
-            print(results)
+
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        NetworkLoader.loadAllPokemon { (allPokemon, errorMsg) in
+            if let allPokemon = allPokemon,
+                let decodedPokemon = PokemonDecoder.decode(data: allPokemon) {
+                PokemonStore.allPokemon = decodedPokemon
+            }
+            if let errorMsg = errorMsg {
+                // TODO: inform user
+                print("Network error: \(errorMsg)")
+            }
         }
+    }
+    
+    // MARK: - Table View
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return PokemonStore.allPokemon.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        let pokemon = PokemonStore.allPokemon[indexPath.row]
+        
+        cell.textLabel!.text = pokemon["name"]
+        return cell
     }
 }
 
