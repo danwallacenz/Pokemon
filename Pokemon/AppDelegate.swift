9463b61f5f3ab8a6fce30984576db869cfa700bb
Dan Wallace <daniel.wallace.nz@gmail.com>
2017-09-01 23:03:54 +1200

    add color

--- Pokemon/AppDelegate.swift
+++ Pokemon/AppDelegate.swift
@@ -16,26 +16,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
-        //loadDecodeStorePokemon()
+        UISearchBar.appearance().barTintColor = .pokemonPurple
+        UISearchBar.appearance().tintColor = .white
+        UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).tintColor = .pokemonPurple
+        
         return true
     }
-
-    private func loadDecodeStorePokemon() {
-        NetworkLoader.loadAllPokemon { (allPokemon, errorMsg) in
-            guard errorMsg == nil else {
-                // TODO: inform user
-                print("Network error: \(errorMsg!)"); return
-            }
-            guard let allPokemon = allPokemon,
-                let decodedPokemon = PokemonDecoder.decode(data: allPokemon) else {
-                    print("No results? Decoding error?"); return }
-            
-            // TODO: debugging-remove
-            if let results = String(data: allPokemon, encoding: .utf8) {
-                print(results)
-            }
-            PokemonStore.allPokemon = decodedPokemon
-        }
-    }
 }
+
+extension UIColor {
+    static let pokemonPurple = UIColor(red: 180.0/255.0, green: 105.0/255.0, blue: 181.0/255.0, alpha: 1.0)
+}
+
+//
+//    private func loadDecodeStorePokemon() {
+//        NetworkLoader.loadAllPokemon { (allPokemon, errorMsg) in
+//            guard errorMsg == nil else {
+//                // TODO: inform user
+//                print("Network error: \(errorMsg!)"); return
+//            }
+//            guard let allPokemon = allPokemon,
+//                let decodedPokemon = PokemonDecoder.decode(data: allPokemon) else {
+//                    print("No results? Decoding error?"); return }
+//
+//            // TODO: debugging-remove
+//            if let results = String(data: allPokemon, encoding: .utf8) {
+//                print(results)
+//            }
+//            PokemonStore.allPokemon = decodedPokemon
+//        }
+//    }
+//}
 
