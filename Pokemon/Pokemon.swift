50cce2e968f4770db855773f5edc906eff23e18c
Dan Wallace <daniel.wallace.nz@gmail.com>
2017-09-02 17:27:43 +1200

    provide detailVC with pPokemon ID

---
+++ Pokemon/Pokemon.swift
@@ -0,0 +1,17 @@
+//
+//  Pokemon.swift
+//  Pokemon
+//
+//  Created by Daniel Wallace on 2/09/17.
+//  Copyright © 2017 danwallacenz. All rights reserved.
+//
+
+import Foundation
+
+struct Pokemon {
+    let id: String
+    let name: String
+    let weight: Int
+    let height: Int
+    let img: URL
+}
