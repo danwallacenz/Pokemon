3b44af3fb6f45c0030ebc3fb4930f500901548a3
Dan Wallace <daniel.wallace.nz@gmail.com>
2017-09-02 15:21:44 +1200

    extract base url, alter data structure

--- PokemonTests/PokemonDecoderTest.swift
+++ PokemonTests/PokemonDecoderTest.swift
@@ -26,7 +26,24 @@ class PokemonDecoderTest: XCTestCase {
         
         XCTAssertEqual(decodedCount, decoded!.count)
         
-        guard let first = decoded?.first, first.count == 1 else { XCTFail("Weird"); return }
-        guard let _ = Int(first.values.first!) else {XCTFail("Bad key (not an Int)"); return} // needed? assumes key is an Int
+//        guard let first = decoded?.first, first.count == 1 else { XCTFail("Weird"); return }
+//        guard let _ = Int(first.values.first!) else {XCTFail("Bad key (not an Int)"); return} // needed? assumes key is an Int
+    }
+    
+    func testDecodeAll2() {
+        
+        // given
+        guard let testData = PokemonTestData.all.data(using: .utf8) else { XCTFail("No test data"); return }
+        
+        // when
+        let decoded = PokemonDecoder.decode2(data: testData)
+        
+        // then
+        XCTAssertNotNil(decoded)
+        XCTAssertEqual(4, decoded!.baseURL.pathComponents.count)
+        XCTAssertEqual("https://pokeapi.co/api/v2/pokemon/", decoded!.baseURL.absoluteString)
+        
+        guard let decodedCount = PokemonDecoder.decodeCount(from: testData) else { XCTFail("No count"); return }
+        XCTAssertEqual(decodedCount, decoded!.data.count)
     }
 }
